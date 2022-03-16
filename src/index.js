7e55c80c6b745ccd023704dfda71e79304af6728
Gordon home PC <aardappel156@gmail.com>
2022-03-16 19:18:31 +0100

    Dropped object colliding

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
-import Phaser from 'phaser';
+import Phaser, { Physics } from 'phaser';
 import logoImg from './assets/logo.png';
 import pizzaBox from './assets/nyp-pizzabox.png';
 import platform from './assets/platform.png';
 
+var groundPlatform
+var inventoryBoxes = []
+var amount = 3;
+var current = 0;
 
+var stackBlock;
 class MyGame extends Phaser.Scene
 {    
 
@@ -16,8 +21,8 @@ class MyGame extends Phaser.Scene
 
     preload ()
     {
+        // load iamges
         this.load.image('logo', logoImg);
-
         this.load.image('pizzaBox', pizzaBox);
         this.load.image('platform', platform);
     }
@@ -26,9 +31,8 @@ class MyGame extends Phaser.Scene
     
     create ()
     {
+        // create essentials stuff
         var pBox = this.add.image(100, 100, 'pizzaBox');
-        //var pForm = this.add.image('platform');
-        //var logo = this.add.image(100, 100,'logo');
         var tween = this.tweens.add({
             targets: pBox,
             x: 700,
@@ -36,19 +40,42 @@ class MyGame extends Phaser.Scene
             yoyo: true,
             repeat: -1
         });
+        groundPlatform = this.physics.add.staticGroup();
+        groundPlatform.create(400, 550, 'platform');
 
+        var stackBlock = this.physics.add.group();
+
+
+        // populate array
+        for(var i = 0; i < amount; i++)
+        {
+            inventoryBoxes[i] = 'pizzaBox';
+        }
+
+
+        
+        // input
         var phy = this.physics
         this.input.on('pointerdown', function()
         {
-            tween.stop();
+            //tween.stop();
 
-            console.log(pBox.x + " " + pBox.y)
-            phy.add.sprite(pBox.x, pBox.y, 'platform')
+            if(current > inventoryBoxes.length)
+            {
+                console.log("Reached max")
+                return;
+            }
+
+            stackBlock.create(pBox.x, pBox.y, 'pizzaBox')
+
+
+            current++;
         });
 
-        var platform = this.physics.add.staticGroup();
-        platform.create(400, 550, 'platform');
+        // add physics
         this.physics.add.collider(pBox, platform);
+        this.physics.add.collider(stackBlock, groundPlatform)
+        this.physics.add.collider(stackBlock, stackBlock)
     }
 
 
