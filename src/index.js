140aaf5d085158abee283c023814156ca8e1c857
Gordon home PC <aardappel156@gmail.com>
2022-03-16 18:39:44 +0100

    working pingpong
    WIP dropping the object

--- src/index.js
+++ src/index.js
@@ -1,36 +1,73 @@
 import Phaser from 'phaser';
 import logoImg from './assets/logo.png';
+import pizzaBox from './assets/nyp-pizzabox.png';
+import platform from './assets/platform.png';
+
 
 class MyGame extends Phaser.Scene
-{
+{    
+
     constructor ()
     {
         super();
     }
 
+    
+
     preload ()
     {
         this.load.image('logo', logoImg);
+
+        this.load.image('pizzaBox', pizzaBox);
+        this.load.image('platform', platform);
     }
-      
+    
+
+    
     create ()
     {
-        const logo = this.add.image(400, 150, 'logo');
-      
-        this.tweens.add({
-            targets: logo,
-            y: 450,
+        var pBox = this.add.image(100, 100, 'pizzaBox');
+        //var pForm = this.add.image('platform');
+        //var logo = this.add.image(100, 100,'logo');
+        var tween = this.tweens.add({
+            targets: pBox,
+            x: 700,
             duration: 2000,
-            ease: "Power2",
             yoyo: true,
-            loop: -1
+            repeat: -1
         });
+
+        this.input.on('pointerdown', function()
+        {
+            tween.stop();
+
+            console.log(pBox.x + " " + pBox.y)
+            //this.physics.add(100, 100, pBox);
+        });
+
+        var platform = this.physics.add.staticGroup();
+        platform.create(400, 550, 'platform');
+        this.physics.add.collider(pBox, platform);
     }
+
+
+    update()
+    {
+        //pBox.x = Phaser.Math.Easing.Sine()
+    }
+
 }
 
 const config = {
     type: Phaser.AUTO,
     parent: 'phaser-example',
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
     width: 800,
     height: 600,
     scene: MyGame
